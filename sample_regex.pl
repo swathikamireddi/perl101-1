e5599b84d7a87964f2cd4c447f3bc9649854dfd4
BHARATH CHEKURI <bchekuri@outlook.com>
2019-02-16 11:02:26 -0600

    Adding Regex in perl

---
+++ sample_regex.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $my_str = 'Options allow-from="www.google.com|  msb.waf.wer.net |www.bc.com|msb.waf.wer.net|msb.test.king.net|tmb.test.king.net.extra",  command="ls -ltr"';
+my $match_str = '';
+
+# m/\s*allow-from\s*={1}\s*(['\"]).+?\1/
+
+if ( $my_str =~ m/\s*allow-from\s*={1}\s*(["]).+?\1/ ) {
+    print "Before Match - $` \n";
+    my $match_val = $&;
+    print "Match Out - $match_val \n";
+    print "After Match - $' \n";
+    if ($match_val =~ m/".*"/) {
+        print "Match Out 2- $& \n";
+    }
+}
+
+print "\n\n\n";
+
+if ( $my_str =~ m/\s*\Q$match_str\E\|?/i ) {
+    print "Match found\n";
+    print "Input - $my_str\n";
+    $my_str =~ s/\s*\Q$match_str\E\|?//gi ;
+    print "Final Output 1 - $my_str \n";
+    $my_str =~ s/\s*\|+\s*"\s*$/"/i;
+    print "Final Output 2 - $my_str \n";
+}
